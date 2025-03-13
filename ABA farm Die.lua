dc13839433ccdb10262535999da14f3985dff6ef
Ornatepuma <124937418+Ornatepuma@users.noreply.github.com>
2025-03-13 01:14:07 -0500

    Update ABA farm Die.lua

--- ABA farm Die.lua
+++ ABA farm Die.lua
@@ -9,17 +9,13 @@ local resetLoopActive = false
 local deaths = game:GetService("Players").LocalPlayer.leaderstats.Deaths
 local waitTime = 9 -- for einstein change this to higher number and the one on line 56 and 30
 _G.Play = false
-local NoAFK = false
 
 print("executed DEATH", player.Name)
 
-local function AntiAfk()
-    if NoAFK == true then
-        while NoAFK do
-            game:GetService("ReplicatedStorage"):WaitForChild("RematchVote"):FireServer()
-
-        end
-    end
+local function AFK()
+    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Two, false, nil) -- Press "2"
+    wait(0.1)
+    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Two, false, nil) -- Release "2"
 end
 
 local function Reset()
@@ -34,6 +30,7 @@ local function Reset()
                 wait(1)
                 game:GetService("ReplicatedStorage"):WaitForChild("RematchVote"):FireServer()
                 print("Voted rematch")
+                AFK()
 
             elseif deaths.Value >= 4 or deaths.Value <= 4 then
             
@@ -43,9 +40,6 @@ local function Reset()
                     waitTime = 4 -- <<<<<<<<<<< THIS ONE
                     print("Killed and set the wait time", resetLoopActive)
                     wait(1)
-                    VirtualInputManager:SendKeyEvent(true, Enum.KeyCode.Two, false, nil) -- Press "2"
-                    wait(0.1)
-                    VirtualInputManager:SendKeyEvent(false, Enum.KeyCode.Two, false, nil) -- Release "2"
                 end
             end
         end
@@ -61,10 +55,8 @@ end
 -- Detect when "Map" is added
 workspaceService.ChildAdded:Connect(function(child)
     if child.Name == "Map" then
-        print("Map Spawned antiafk and reset")
         Reset()
-        NoAFK = true
-        AntiAfk()
+        print("Map Spawned antiafk and reset")
     end
 end)
 
@@ -74,6 +66,6 @@ workspaceService.ChildRemoved:Connect(function(child)
         stopResetLoop()  -- Stop the loop when map is removed
         waitTime = 9 -- <<<<<<<<<<   THIS ONE
         print("Stopping reset loop and changed respawn time", waitTime)
-        NoAFK = false
     end
 end)
+
