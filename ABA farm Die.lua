627a5fc583408ae93fd17884402e1ec2a0228327
Ornatepuma <124937418+Ornatepuma@users.noreply.github.com>
2025-05-03 12:49:36 -0500

    Update ABA farm Die.lua

--- ABA farm Die.lua
+++ ABA farm Die.lua
@@ -1,67 +1,30 @@
 -- DEATH DEATH DEATH DEATH DEATH DEATH
 -- DEATH DEATH DEATH DEATH DEATH DEATH
 
-local VirtualInputManager = game:GetService("VirtualInputManager")
-local Live = game.Workspace.Live
-local player = game.Players.LocalPlayer
-local workspaceService = game:GetService("Workspace")
-local resetLoopActive = false 
-local deaths = game:GetService("Players").LocalPlayer.leaderstats.Deaths
-local waitTime = 9 -- for einstein change this to higher number and the one on line 56 and 30
-_G.Play = false
-
-print("executed DEATH", player.Name)
-
-for i,v in pairs(getconnections(game.Players.LocalPlayer.Idled)) do
-    v:Disable()
+function Rematch()
+    game:GetService("ReplicatedStorage"):WaitForChild("RematchVote"):FireServer()
 end
 
-local function Reset()
-    if _G.Play == false and resetLoopActive == false then
-        resetLoopActive = true
-        _G.Play = true
-        while resetLoopActive do
-            wait(.3)
 
-            -- Check for death count
-            if deaths.Value == 4 then
-                wait(1)
-                game:GetService("ReplicatedStorage"):WaitForChild("RematchVote"):FireServer()
-                print("Voted rematch")
-
-            elseif deaths.Value >= 4 or deaths.Value <= 4 then
-            
-                if player.Character and player.Character:FindFirstChild("Humanoid") and resetLoopActive == true then
-                    wait(waitTime)
-                    game.Players.LocalPlayer.Character.Humanoid.Health = 0
-                    waitTime = 4 -- <<<<<<<<<<< THIS ONE
-                    print("Killed and set the wait time", resetLoopActive)
-                    wait(1)
-                end
-            end
+workspace.ChildAdded:Connect(function(c)
+    if c.Name == "Map" then 
+        if game.Players.LocalPlayer.Character and game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") then 
+            game.Players.LocalPlayer.Character.Humanoid.Health = 0
+            game.Players.LocalPlayer.Character.Humanoid:ChangeState(3)
         end
     end
-end
-
--- Function to stop the reset loop
-local function stopResetLoop()
-    resetLoopActive = false
-    _G.Play = false
-end
-
--- Detect when "Map" is added
-workspaceService.ChildAdded:Connect(function(child)
-    if child.Name == "Map" then
-        Reset()
-        print("Map Spawned antiafk and reset")
+end)
+game.Players.LocalPlayer.PlayerGui.ChildAdded:Connect(function(v)
+    if v.Name == "BanChooser" then 
+        v:WaitForChild("rem"):FireServer("pass")
     end
 end)
-
--- Detect when "Map" is removed
-workspaceService.ChildRemoved:Connect(function(child)
-    if child.Name == "Map" then
-        stopResetLoop()  -- Stop the loop when map is removed
-        waitTime = 9 -- <<<<<<<<<<   THIS ONE
-        print("Stopping reset loop and changed respawn time", waitTime)
+game.Players.LocalPlayer.CharacterAdded:Connect(function()
+    if workspace:FindFirstChild("Map") then 
+        game.Players.LocalPlayer.Character:WaitForChild("Humanoid").Health = 0
+        game.Players.LocalPlayer.Character.Humanoid:ChangeState(3)
+        pcall(function()
+            game:GetService("ReplicatedStorage"):WaitForChild("RematchVote"):FireServer()
+        end)
     end
 end)
